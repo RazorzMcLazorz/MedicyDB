4484e2a055506ddc70245d768c7850cc56193acd
RazorzMcLazorz <young.nathaniel95@gmail.com>
2020-04-07 17:57:27 -0600

    User INformation added and stored on MYSQL QUERYS

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 // mysql://bb0380b28b7a16:01e2429c@us-cdbr-iron-east-01.cleardb.net/heroku_f74e2220185cbc5?reconnect=true
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql");
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-console.log("server started");
+console.log('server started');
 
 const db_config = {
-  host: "us-iron-auto-dca-01-b-bh.cleardb.net",
-  user: "b274c165001cb4",
-  password: "ad1f9f41",
-  database: "heroku_66d409414a572d7"
+  host: 'us-iron-auto-dca-01-b-bh.cleardb.net',
+  user: 'b274c165001cb4',
+  password: 'ad1f9f41',
+  database: 'heroku_66d409414a572d7'
 }
 
 var con
@@ -36,8 +36,50 @@ con.connect(function(err){
 });
 
 // grab user data
-app.get('/user', (req, res) => {
-  con.query('SELECT * FROM user;', (err, result) => {
+app.get('/users', (req, res) => {
+  con.query(`SELECT * FROM users;`, (err, result) => {
+    if (err) {
+      return res.send(err);
+    }
+    else {
+      return res.json({
+        data : result
+      })
+    }
+  });
+});
+
+app.get('/users/login', (req, res) => {
+  const { name, pass } = req.query;
+  con.query(`SELECT * FROM users WHERE users_username='${name}', users_password='${pass}';`, (err, result) => {
+    if (err) {
+      return res.send(err);
+    }
+    else {
+      return res.json({
+        data : result
+      })
+    }
+  });
+});
+
+app.get('/users/select', (req, res) => {
+  const { name } = req.query;
+  con.query(`SELECT * FROM users WHERE users_username='${name}';`, (err, result) => {
+    if (err) {
+      return res.send(err);
+    }
+    else {
+      return res.json({
+        data : result
+      })
+    }
+  });
+});
+
+app.get('/users/add', (req, res) => {
+  const { name, pass, perm } = req.query;
+  con.query(`INSERT INTO users SET users_username='${name}', users_password='${pass}', users_permissions='${perm}';`, (err, result) => {
     if (err) {
       return res.send(err);
     }
@@ -64,3 +106,7 @@ app.listen(port, () => {
 });
 
 handleDisconnect();
+
+/**
+ * updating this file requires a restart, as its not constantly watching.
+ */
